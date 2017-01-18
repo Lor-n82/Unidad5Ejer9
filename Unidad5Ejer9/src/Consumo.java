4b0a7f3905d7909c4512814450f69ab48f9cc87e
admin1 <admin1@PC1414>
2017-01-18 14:15:32 +0100

    Segunda subida

--- Unidad5Ejer9/src/Consumo.java
+++ Unidad5Ejer9/src/Consumo.java
@@ -9,5 +9,31 @@
  * @author admin1
  */
 public class Consumo {
-    
+    //atributos
+        private double kms;
+        private double litros;
+        private double vmed;
+        private String tipocombustible;
+    //constructores
+
+    public Consumo(double kms, double litros, double vmed, String tipocombustible) {
+        this.kms = kms;
+        this.litros = litros;
+        this.vmed = vmed;
+        this.tipocombustible = tipocombustible;
+    }
+ 
+        
+        
+    //metodos
+        public double getTiempo(){
+        return kms/vmed;
+        }
+        public double consumoMedio(){
+        return 
+        }
+        public double consumoEuros(){
+        
+        }
+        
 }
